64664b29d204e0460390f3351f8aa4be2c80fc2b
mazzwell <137079218+mazzwell@users.noreply.github.com>
2024-02-04 18:54:23 +0600

    Update auth0.tsx

--- src/auth0.tsx
+++ src/auth0.tsx
@@ -1,9 +1,19 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated, user } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return (
+    <div>
+      {isAuthenticated ? (
+        <div>
+          <p>Welcome, {user.name}!</p>
+        </div>
+      ) : (
+        <button onClick={() => loginWithRedirect()}>Log In</button>
+      )}
+    </div>
+  );
 };
 
 export default LoginButton;
