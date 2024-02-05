27af3b1e8dd7251a1efdf2c22e3446a6f5d2835e
mazzwell <137079218+mazzwell@users.noreply.github.com>
2024-02-05 12:36:05 +0600

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { auth, twitterProvider } from './firebase';
+import {  auth, twitterProvider } from './firebase';
+import { User } from 'firebase/auth';
 import Task from './task';
 
 const TwitterLoginButton: React.FC = () => {
