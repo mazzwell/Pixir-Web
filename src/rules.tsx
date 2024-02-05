eca5622c309bf9ec53662def141539882cfbbc8e
mazzwell <137079218+mazzwell@users.noreply.github.com>
2024-02-05 16:06:35 +0600

    mobile

--- src/rules.tsx
+++ src/rules.tsx
@@ -4,7 +4,7 @@ function Section2() {
   return (
     <div className="pagecontainer">
       <h1>Rules</h1>
-      <p>Collect as many NFTs as you can and secure a place in the Top 10 leaderboard to win a share of 500k $Pixir tokens. <br></br> We will distribute 1k-10K NFTs for free each month, but each user can only have one per wallet. The game aims to find all the NFTs, and the top holder will receive a share of 500K $pixir. The remaining 500k $pixir will be distributed proportionally to the number of NFTs held, with a minimum requirement of 10 NFTs."</p>
+      <p>Collect as many NFTs as you can and secure a place in the Top 10 leaderboard to win a share of 500k $Pixir tokens. </p> <p> We will distribute 1k-10K NFTs for free each month,but each user can only have one per wallet.  </p> <p> The game aims to find all the NFTs, and the top holder will receive a share of 500K $pixir. </p> <p> The remaining 500k $pixir will be distributed proportionally to the number of NFTs held,  </p> <p> with a minimum requirement of 10 NFTs.</p>
     </div>
   );
 }
