ad555c93bb09eaf8561be8163ac8e22291c3d502
mazzwell <137079218+mazzwell@users.noreply.github.com>
2024-02-10 22:45:45 +0600

    Update shop.tsx

--- src/shop.tsx
+++ src/shop.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 function Section3() {
-  const [inputValue, setInputValue] = useState<number>(100000); // Initial value set to 5000
 
   const web: React.CSSProperties = {
     color: "#fff",
@@ -14,10 +13,6 @@ function Section3() {
     padding: "15px",
   };
 
-  // Adjusted to handle slider input
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(Number(e.target.value));
-  };
 
   return (
     <>
@@ -34,21 +29,10 @@ function Section3() {
             <h1> Pixir Token </h1>
             <p> 30% discount for early birds </p>
             <a> Price: 0.001 MATIC </a>
-            <br></br>
-            <br></br>
-            <input
-              type="range"
-              min="1000" // Minimum value for the slider
-              max="1000000" // Maximum value for the slider
-              value={inputValue}
-              onChange={handleInputChange}
-              style={{ width: '100%', marginBottom: '20px' }} // Adjusted styling for better usability
-            />
-            <div>Selected amount: {inputValue.toLocaleString()}</div> {/* Display the selected amount */}
             <br/>
-            <a
+            <a href="https://morkie.xyz/pixir"
                 style={web}
-              > Coming&nbsp;&nbsp;Soon
+              > Buy&nbsp;&nbsp;$Pixir
             </a>
           </div>
         </div>
